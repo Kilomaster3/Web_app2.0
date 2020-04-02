9c84c31cd164802d67485ba0ce0a3aa78d7122b8
Kilomaster_3 <hromalex.2011@gmail.com>
2020-04-02 16:33:07 +0300

    Change User Profile

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], secure_image_url: true , :scope => 'r_liteprofile'
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], secure_image_url: true, scope: 'r_liteprofile'
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECERT'], secure_image_url: true
 end
