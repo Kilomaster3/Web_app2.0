b4de96992fce7ce95b2fefc282bdbb18eccb8fed
Kilomaster_3 <hromalex.2011@gmail.com>
2020-04-03 09:00:44 +0300

    Add Setting

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,20 @@
+class AccountsController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+   end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
     get '/auth/facebook/callback' => 'users/omniauth_callbacks#facebook'
     get '/auth/linkedin/callback' => 'users/omniauth_callbacks#linkedin'
+
+    resources :accounts 
   end
 end
