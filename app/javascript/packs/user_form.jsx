0b73ae25d5e54b4e8d055bf1a1349259e82f0ad9
Kilomaster_3 <hromalex.2011@gmail.com>
2020-04-10 16:40:21 +0300

    Add Username

--- app/javascript/packs/user_form.jsx
+++ app/javascript/packs/user_form.jsx
@@ -1,32 +1,62 @@
 import React from 'react'
-class UserForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+class UserForm extends React.Component{
+  state = {
+    login: '',
+    password: '',
+  };
+  
+  handleChange = e => {
+    const { name, value } = e.target;
+    
+    this.setState({
+      [name]: value,
+    });
+  };
+  
+  handleSubmit = e => {
+    const { login, password } = this.state;
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    e.preventDefault();
+    alert(
+      login: ${login}
+      password: ${password}
+    );
+  };
+  
+  render(){
+    const { login, password } = this.state;
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-
-        <input type="submit" value="Submit" />
-      </form>
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <Fields
+            text="Введите логин"
+            type="text"
+            name="login"
+            value={login}
+            onChange={this.handleChange}
+           />
+          <Fields
+            text="Введите пароль"
+            type="password"
+            name= "password"
+            value={password}
+            onChange={this.handleChange}
+           />
+          <MuiThemeProvider>
+            <RaiseButtin
+              label="Войти"
+              fullWidth={true}
+              style={style.button}
+             >
+               <input
+                 type="submit"
+                 style={style.exampleImageInput}
+               />
+             </RaiseButtin>
+          </MuiThemeProvider>
+        </form>
+      </div>
     );
   }
 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         authentication_keys: [:username]
+         authentication_keys: [:username, :email]
 
   devise :omniauthable, omniauth_providers: [:facebook, :linkedin]
   validates :email, uniqueness: true
@@ -21,8 +21,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.name = auth.info.name
+      user.image = auth.info.image
     end
   end
 end
@@ -35,9 +35,9 @@ def self.connect_to_linkedin(auth, _signed_in_resource = nil)
     registered_user = User.where(email: auth.info.email).first
     registered_user || user = User.create(name: auth.info.first_name, provider: auth.provider, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0, 20])
  end
- def email_required?
+  def email_required?
     false
-  end
+   end
 
   def email_changed?
     false
